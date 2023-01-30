5a1d105106525aa6e76f3ac8b3d6374a82c7a5fc
bellotti.gui@outlook.com <bellotti.gui@outlook.com>
2023-01-30 20:01:21 -0300

    02 - methods and headers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,28 @@
 import http from 'http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
+
+    const { method, url } = req
+
+    if (method === "GET" && url === "/users") {
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+
+    if (method === "POST" && url === "/users") {
+
+        users.push({
+            id: 1,
+            name: "Guilherme",
+            email: "lala@email.com"
+        })
+
+        return res.end('Criação de usuário')
+    }
+
     return res.end('hello world')
 })
 
